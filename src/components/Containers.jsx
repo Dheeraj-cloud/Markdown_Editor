151c4bebf0f5704b9a7a0241828d574916457b4e
dheeraj0206 <dheeraj02062000@gmail.com>
2023-03-27 14:34:56 +0530

    Non-functional-ui

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -5,8 +5,15 @@ const Containers = () => {
     <div>
       <div className={"container-fluid"}>
         <div className={"row"}>
-          <div className={"col-lg-6 bg-primary"}> <h1> Markdown </h1></div>
-          <div className={"col-lg-6 bg-success"}>  <h1> Preview </h1> </div>
+          <div className={"col-lg-6 bg-primary"}>
+            {" "}
+            <h1> Markdown </h1>
+            <textarea name="text-area" id="" cols="60" rows="25"></textarea>
+          </div>
+          <div className={"col-lg-6 bg-success"}>
+            {" "}
+            <h1> Preview </h1>{" "}
+          </div>
         </div>
       </div>
     </div>
