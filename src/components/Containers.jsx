1db06fbdbf11d0fc6192473937f438e2365e53a9
dheeraj0206 <dheeraj02062000@gmail.com>
2023-03-27 14:29:10 +0530

    feat/UI-Skeleton

---
+++ src/components/Containers.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Containers = () => {
+  return (
+    <div>
+      <div className={"container-fluid"}>
+        <div className={"row"}>
+          <div className={"col-lg-6 bg-primary"}> <h1> Markdown </h1></div>
+          <div className={"col-lg-6 bg-success"}>  <h1> Preview </h1> </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Containers;
